13fda458f5283819d3a0820f184c0bdff409deeb
Phathutshedzo Netshifhefhe <phathutshedzon@wolfware.co.za>
2025-05-23 12:07:03 +0200

    Thane

--- ClassroomFrontend/src/pages/ClassroomsPage.tsx
+++ ClassroomFrontend/src/pages/ClassroomsPage.tsx
@@ -1,6 +1,22 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function ClassroomsPage() {
+
+  const [FormData,setForm] = useState({
+    
+    Name:'',
+    Capacity:''
+  });
+
+  const [msg,setmsg] = useState();
+
+  const handleSubmit = async(e) =>{
+    e.prevantDefault;
+  
+
+  const response  = await.axios.post("url")
+
 
 return (
     <div className="container mt-4">
